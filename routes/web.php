bf12cbe424adea5dd75fec597f4894f763de0b7f
Arthur-Rodriguess <artrod150309@gmail.com>
2026-05-31 15:00:22 -0300

    feat(routes): adiciona rota para concluir tarefa

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompleteTaskController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,4 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::get('/tasks/create', [TaskController::class, 'create']);
 Route::post('/tasks/save', [TaskController::class, 'store']);
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+Route::post('/tasks/{task}', CompleteTaskController::class);
