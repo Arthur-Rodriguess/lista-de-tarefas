582d53da668629a099485390e2a75a9d549ddf7d
Arthur-Rodriguess <artrod150309@gmail.com>
2026-05-31 14:23:20 -0300

    feature(database): adiciona timestamps as tarefas para apresentar elas em ordem decrescente de criacao

--- database/migrations/2026_05_30_012318_create_table_tasks.php
+++ database/migrations/2026_05_30_012318_create_table_tasks.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 128);
             $table->boolean('concluded')->default(false);
+            $table->timestamps();
         });
     }
 
