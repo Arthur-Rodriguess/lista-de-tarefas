af212d83f0d5cea5bbbf8fac315e5c01c37ee759
Arthur-Rodriguess <artrod150309@gmail.com>
2026-06-10 22:05:07 -0300

    feat(view): adiciona componente de modal de exclusao de tarefa

---
+++ resources/views/components/delete-modal.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade mt-10" tabindex="-1" id="delete-modal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-4">Excluir tarefa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="fs-5">Excluir <strong class="text-danger" id="task-title"></strong>?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-danger" id="confirm-delete">Confirmar</button>
+            </div>
+        </div>
+    </div>
+</div>
