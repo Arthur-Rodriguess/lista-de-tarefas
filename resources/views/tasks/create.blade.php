c4845700e4065f9a6c6aa318a101179e23378f63
Arthur-Rodriguess <artrod150309@gmail.com>
2026-06-10 21:52:35 -0300

    feat(view): adiciona autofocus no input de criacao de tarefa

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,7 +2,7 @@
     <form action="{{ route("tasks.store") }}" method="post">
         <div class="mb-3">
             <label for="title" class="form-label fw-semibold fs-5">Título da tarefa</label>
-            <input type="text" name="title" id="title" required class="form-control">
+            <input type="text" name="title" id="title" required class="form-control" autofocus>
         </div>
         <button type="submit" class="btn btn-primary">Adicionar</button>
     </form>
